91bdbc1b5c5f88d60a6e0641917ba8767c16db73
edmondchui <chui.hinwai@gmail.com>
2019-05-04 02:00:54 -0600

    *update TypeScript definition

--- src/dispatchable.d.tsx
+++ src/dispatchable.d.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
-declare function makeDispatchable<P>(Comp?: React.ComponentType<P>): [makeDispatchable.DispatchReturnType<P>, React.ComponentType<P>];
+// May 4 2019 https://medium.com/dailyjs/typescript-create-a-condition-based-subset-types-9d902cea5b8c
+type SubType<Base, Condition> = Pick<Base, {
+  [Key in keyof Base]: Base[Key] extends Condition ? Key : never
+}[keyof Base]>;
 
-declare namespace makeDispatchable {
-  export type DispatchReturnType<P> = (propKey: keyof P, ...params) => void;
-}
+type FunctionProps<P> = SubType<P, Function>;
+
+declare function makeDispatchable<P>(Comp?: React.ComponentType<P>)
+  : [FunctionProps<P>, React.ComponentType<P>];
 
 export default makeDispatchable;
